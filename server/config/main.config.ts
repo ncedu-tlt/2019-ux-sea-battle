05ca3fd99319f50921168dee2937714dacf925f7
Alexandr <awpogodin@mail.ru>
2020-03-26 11:10:41 +0400

    1 // registration new user & auth

--- server/config/main.config.ts
+++ server/config/main.config.ts
@@ -1,5 +1,5 @@
 export const mainConfig = (): any => ({
-    port: process.env.PORT_SERVER || 3000,
+    port: process.env.PORT || 3000,
     dbUrl: process.env.DATABASE_URL,
     tokenSecretKey: process.env.TOKEN_SECRET_KEY
 });
