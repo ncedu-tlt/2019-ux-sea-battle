ce6045264a241d4c9ec37587cc2faefd0f35421c
eight.8 <phenixfanikukrus@gmail.com>
2020-04-13 21:12:37 +0400

    more args

--- server/decorators/role.decorator.ts
+++ server/decorators/role.decorator.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 import { RolesGuard } from "../guards/roles.guard";
 import { RoleEnum } from "../modules/db/domain/role.enum";
 
-export function Roles(roles: RoleEnum) {
+export function Roles(...roles: RoleEnum[]) {
     return applyDecorators(
         SetMetadata("roles", roles),
         UseGuards(AuthGuard(), RolesGuard)
