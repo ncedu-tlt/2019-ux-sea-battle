84caa56b702d5850cd15b457b4c311d1e8fb4ee2
eight.8 <phenixfanikukrus@gmail.com>
2020-04-20 14:07:00 +0400

    deleted unused func

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,18 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { UsersApiService } from "client/app/services/api/users.api.service";
-import { Observable } from "rxjs";
-import { UserDTO } from "common/dto/user.dto";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "sb-root",
     templateUrl: "./app.component.html"
 })
-export class AppComponent implements OnInit {
-    users: Observable<UserDTO[]>;
-
-    constructor(private usersApiService: UsersApiService) {}
-
-    ngOnInit(): void {
-        this.users = this.usersApiService.getAll();
-    }
-}
+export class AppComponent {}
