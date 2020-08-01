0974ae677cde4691a69df6e2aaa93ce3f17e6a53
eight.8 <phenixfanikukrus@gmail.com>
2020-08-01 18:03:51 +0400

    updating game status

--- server/modules/game/game.controller.ts
+++ server/modules/game/game.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    UseGuards,
+    Request,
+    Patch,
+    Param,
+    Body
+} from "@nestjs/common";
 import { GameService } from "./game.service";
 import { GameDto } from "../../../common/dto/game.dto";
 import { AuthGuard } from "@nestjs/passport";
+import { UpdateGameDto } from "../../../common/dto/update-game.dto";
+import { GameDAO } from "../db/domain/game.dao";
 
 @Controller("/api/games")
 export class GameController {
@@ -12,5 +22,15 @@ export class GameController {
     async getCurrentGame(@Request() req): Promise<GameDto> {
         const user = req.user;
         return await this.gameService.getGameByUserId(user.id);
+    }
+
+    @UseGuards(AuthGuard())
+    @Patch()
+    async update(
+        @Param("id") id: number,
+        @Body() game: UpdateGameDto
+    ): Promise<GameDAO> {
+        game.id = Number(id);
+        return this.gameService.updateGame(game);
     }
 }
--- server/modules/game/game.service.ts
+++ server/modules/game/game.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { GameDAO } from "../db/domain/game.dao";
 import { DeleteResult, Repository } from "typeorm";
-import { GameStatusEnum } from "../db/domain/game-status.enum";
+import { GameStatusEnum } from "../../../common/game-status.enum";
 import { GameModeEnum } from "../../../common/game-mode.enum";
 import { GameDto } from "../../../common/dto/game.dto";
 import { SearchDto } from "../../../common/dto/search.dto";
@@ -10,6 +10,7 @@ import { ParticipantService } from "./participant.service";
 import { ParticipantDAO } from "../db/domain/participant.dao";
 import { CreateGameDto } from "../../../common/dto/create-game.dto";
 import { UpdateGameDto } from "../../../common/dto/update-game.dto";
+import { UpdateGameStatusDto } from "../../../common/dto/update-game-status.dto";
 
 @Injectable()
 export class GameService {
@@ -76,5 +77,10 @@ export class GameService {
             }
         }
         return await this.gameRepository.delete(id);
+    }
+
+    async updateGameState(updateDTO: UpdateGameStatusDto): Promise<GameDAO> {
+        await this.gameRepository.update(updateDTO.id, updateDTO);
+        return await this.gameRepository.findOne(updateDTO.id);
     }
 }
