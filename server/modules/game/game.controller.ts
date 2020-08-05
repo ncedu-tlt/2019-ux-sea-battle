15217aae0745a1e14c4690f7b26f6bca784861e5
eight.8 <phenixfanikukrus@gmail.com>
2020-08-05 15:02:23 +0400

    deleted update game status endpoint

--- server/modules/game/game.controller.ts
+++ server/modules/game/game.controller.ts
@@ -1,17 +1,7 @@
-import {
-    Controller,
-    Get,
-    UseGuards,
-    Request,
-    Patch,
-    Param,
-    Body
-} from "@nestjs/common";
+import { Controller, Get, UseGuards, Request } from "@nestjs/common";
 import { GameService } from "./game.service";
 import { GameDto } from "../../../common/dto/game.dto";
 import { AuthGuard } from "@nestjs/passport";
-import { UpdateGameDto } from "../../../common/dto/update-game.dto";
-import { GameDAO } from "../db/domain/game.dao";
 
 @Controller("/api/games")
 export class GameController {
@@ -22,15 +12,5 @@ export class GameController {
     async getCurrentGame(@Request() req): Promise<GameDto> {
         const user = req.user;
         return await this.gameService.getGameByUserId(user.id);
-    }
-
-    @UseGuards(AuthGuard())
-    @Patch()
-    async update(
-        @Param("id") id: number,
-        @Body() game: UpdateGameDto
-    ): Promise<GameDAO> {
-        game.id = Number(id);
-        return this.gameService.updateGame(game);
     }
 }
