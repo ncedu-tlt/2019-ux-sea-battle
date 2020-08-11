b2969ac9dc976bdb9b95075e2f99950c51b599dc
eight.8 <phenixfanikukrus@gmail.com>
2020-08-11 23:15:35 +0400

    participant of the current game

--- server/modules/game/participant.service.ts
+++ server/modules/game/participant.service.ts
@@ -5,6 +5,7 @@ import { Repository } from "typeorm";
 import { UsersService } from "../users/users.service";
 import { UserDAO } from "../db/domain/user.dao";
 import { GameDAO } from "../db/domain/game.dao";
+import { GameStatusEnum } from "../../../common/game-status.enum";
 
 @Injectable()
 export class ParticipantService {
@@ -24,10 +25,18 @@ export class ParticipantService {
 
     async getParticipantByUserId(userId: number): Promise<ParticipantDAO> {
         const user: UserDAO = await this.usersService.findById(userId);
-        return await this.participantRepository.findOne({
-            where: {
-                user: user.id
+        const participants: ParticipantDAO[] = await this.participantRepository.find(
+            {
+                where: {
+                    user: user.id
+                }
             }
-        });
+        );
+        for (const [i, value] of participants.entries()) {
+            const game = await value.game;
+            if (game.status === GameStatusEnum.STARTED) {
+                return participants[i];
+            }
+        }
     }
 }
